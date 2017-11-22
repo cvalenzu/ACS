b194a2a80fa048b5cff38e25620ddc1303dcf760
Jorge Avarias <javarias@alma.cl>
2017-11-22 15:22:27 -0300

    Added initial classes to read from TMCDB to get acs Services config

---
+++ LGPL/CommonSoftware/cdb_rdb/src/alma/acs/tmcdb/utils/AcsStartRemote.java
@@ -0,0 +1,94 @@
+/*
+ * ALMA - Atacama Large Millimeter Array
+ * (c) European Southern Observatory, 2002
+ * (c) Associated Universities Inc., 2002
+ * Copyright by ESO (in the framework of the ALMA collaboration),
+ * Copyright by AUI (in the framework of the ALMA collaboration),
+ * All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY, without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307  USA
+ *
+ */
+package alma.acs.tmcdb.utils;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
+
+import com.cosylab.cdb.jdal.HibernateWDALImpl;
+import com.cosylab.cdb.jdal.hibernate.HibernateUtil;
+
+import alma.acs.tmcdb.AcsService;
+import alma.acs.tmcdb.Computer;
+import alma.acs.tmcdb.Configuration;
+
+public class AcsStartRemote {
+
+	private static final String configName;
+	static {
+		configName = System.getProperty(HibernateWDALImpl.TMCDB_CONFIGURATION_NAME_KEY, 
+				System.getenv(HibernateWDALImpl.TMCDB_CONFIGURATION_NAME_KEY));
+	}
+	
+	private HibernateUtil hibU;
+	private Configuration config;
+	
+	public AcsStartRemote() {
+		hibU = HibernateUtil.getInstance(Logger.getAnonymousLogger());
+		Session session = hibU.getSessionFactory().openSession();
+		try {
+		session.beginTransaction();
+		config = (Configuration)session.createCriteria(Configuration.class).
+				add(Restrictions.eq("configurationName", configName)).uniqueResult();
+		} finally {
+			if (session != null)
+				session.close();
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	public <T> List<T> getListForConfiguration(Session session, Class<T> type)
+	{
+		List<T> result = null;
+		result = session.createCriteria(type.getClass()).add(Restrictions.eq("configuration", config)).list();
+        return result;
+	}
+	
+	public List<AcsService> getServicesDeployment() {
+		Session session = hibU.getSessionFactory().openSession();
+		List<AcsService> services = null;
+		try {
+			session.beginTransaction();
+			services = getListForConfiguration(session, AcsService.class);
+		} finally {
+			if (session != null)
+				session.close();
+		}
+		return services;
+	}
+	
+	public void startServiceAt(AcsService service, Computer computer) {
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
